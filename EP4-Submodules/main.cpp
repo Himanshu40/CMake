5fccceb0bc24b6a650f1542f1882a5bfe8fe3c61
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-09-15 22:31:03 +0530

    Added submodules check to see git exists or not

--- EP4-Submodules/main.cpp
+++ EP4-Submodules/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 #include "adder.h"
+
 
 int main() {
     std::cout << "Hello world!!!" << std::endl;
     std::cout << add(12.5f, 52.36f) << std::endl;
+
+    GLFWwindow *window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
 
     return 0;
 }
