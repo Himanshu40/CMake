1383e04e7d40e487b2f1492128c49d3b9867e1a7
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-09-22 13:10:22 +0530

    Uploaded Episode 8 installing with CPack

---
+++ EP8-CPackInstall/README.md
@@ -0,0 +1,8 @@
+# EP-8
+* ### Installing with CPack
+
+## Synopsis
+```cmake
+# Include this module to search for compiler-provided system runtime libraries and add install rules for them.
+include(InstallRequiredSystemLibraries)
+```
---
+++ EP8-CPackInstall/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <VersionConfig.h>
+
+int main() {
+    std::cout << "Version: " << EP8_CPackInstall_VERSION_MAJOR << "." << EP8_CPackInstall_VERSION_MINOR << std::endl;
+    std::cout << "Hello world!!!" << std::endl;
+
+    return 0;
+}
